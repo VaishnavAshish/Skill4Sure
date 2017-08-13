f52d92a7352405c54c87e34f85074b07d37dbd01
ashvaish\Ashish vaishnav <vaishnavashish5@gmail.com>
2017-08-13 11:17:51 +0530

    bfh

---
+++ angular/controller/module.ctrl.js
@@ -0,0 +1,16 @@
+angular.module("starter")
+.controller("moduleCtrl",function(module, $http){
+    var home=this;
+    var req = {
+        method:'POST',
+        url: "http://localhost/api_skill4sure/",
+        headers: {
+            Access-Control-Allow-Origin
+        }
+
+
+    }
+
+
+
+})
