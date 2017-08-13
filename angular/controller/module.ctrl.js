5541873c7e4bc6d5b9619cff3319e2e3d8ec4747
Akshay <akshaychauhan7737@gmail.com>
2017-08-13 11:25:44 +0530

    module correction $http request

--- angular/controller/module.ctrl.js
+++ angular/controller/module.ctrl.js
@@ -1,16 +1,18 @@
 angular.module("starter")
-.controller("moduleCtrl",function(module, $http){
-    var home=this;
-    var req = {
-        method:'POST',
-        url: "http://localhost/api_skill4sure/",
-        headers: {
-            Access-Control-Allow-Origin
-        }
+    .controller("moduleCtrl", function ($http) {
+        var home = this;
+        var req = {
+            method: 'POST',
+            url: "http://localhost/api_skill4sure/",
+            headers: {
+                'Access-Control-Allow-Origin': "*"
+            },
+            data: { module: "true" }
+        };
 
+        $http(req).then(
+            function (response) { module.data = response.data },
+            function (error) { console.log(error); }
+        )
 
-    }
-
-
-
-})
+    })
